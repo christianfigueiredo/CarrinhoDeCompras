df8928185f1cc1f1d44c41a1737033abd2c4b450
ChristianFiqueiredo <developer.figueiredo@gmail.com>
2022-05-20 09:07:33 +0100

    criação do projeto mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CarrinhoDeCompras.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
